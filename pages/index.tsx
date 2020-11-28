29b2854a9661dd12647c35c93bedaf9703d59e4c
Emanuele Ricci <stermi@gmail.com>
2020-11-28 08:52:55 +0100

    fixed layout shifting when hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,15 +12,15 @@ import Image from 'next/image';
 
 const IndexPage = () => (
   <Layout title="Home | Emanuele Ricci ~ Full Stack Developer">
-    <div className="grid grid-cols-7 h-screen bg-primary">
+    <div className="grid h-screen grid-cols-7 bg-primary">
       <div className="col-span-4">
-        <div className="w-full h-full relative">
+        <div className="relative w-full h-full">
           <Image className="object-cover" src="/cover.jpeg" layout="fill" />
         </div>
       </div>
-      <div className="col-span-3 flex flex-col justify-center align-middle items-center">
+      <div className="flex flex-col items-center justify-center col-span-3 align-middle">
         <div className="px-32 text-center text-primary">
-          <div className="font-mulish font-black text-7xl">Emanuele Ricci</div>
+          <div className="font-black font-mulish text-7xl">Emanuele Ricci</div>
           <div className="py-10 text-2xl font-light">
             Full Stack Deloper @{' '}
             <a className="font-bold underline" href="https://soplaya.com/">
@@ -28,23 +28,23 @@ const IndexPage = () => (
             </a>{' '}
             / Alexa & Google Assistant Developer
           </div>
-          <div className="pt-10 space-x-8 flex flex-row justify-center">
-            <div className="flex items-center bg-primary hover:border rounded-full p-5 hover:border-button hover:text-button">
+          <div className="flex flex-row justify-center pt-10 space-x-8">
+            <div className="flex items-center p-5 border rounded-full bg-primary border-primary hover:border-button hover:text-button">
               <a href="https://twitter.com/StErMi">
                 <FontAwesomeIcon icon={faTwitter} className="text-3xl" />
               </a>
             </div>
-            <div className="flex items-center bg-primary hover:border rounded-full p-5 hover:border-button hover:text-button">
+            <div className="flex items-center px-6 py-5 border rounded-full bg-primary border-primary hover:border-button hover:text-button">
               <a href="https://www.linkedin.com/in/emanuelericci/">
                 <FontAwesomeIcon icon={faLinkedinIn} className="text-3xl" />
               </a>
             </div>
-            <div className="flex items-center bg-primary hover:border rounded-full p-5 hover:border-button hover:text-button">
+            <div className="flex items-center p-5 border rounded-full bg-primary border-primary hover:border-button hover:text-button">
               <a href="https://github.com/StErMi">
                 <FontAwesomeIcon icon={faGithub} className="text-3xl" />
               </a>
             </div>
-            <div className="flex items-center bg-primary hover:border rounded-full p-5 hover:border-button hover:text-button">
+            <div className="flex items-center px-6 py-5 border rounded-full bg-primary border-primary hover:border-button hover:text-button">
               <a href="https://www.instagram.com/ericci87">
                 <FontAwesomeIcon icon={faInstagram} className="text-3xl" />
               </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         primary: '#515369',
       },
       borderColor: {
+        primary: '#FFFFFFF5',
         button: '#46C7B0',
       },
     },
